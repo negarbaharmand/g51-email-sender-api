153cc60c5d5a65226edf54eac4363d1375fd8de0
Negar Baharmand <negarbaharmand@outlook.com>
2024-10-14 09:05:59 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/g51emailsenderapi/G51EmailSenderApiApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g51emailsenderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G51EmailSenderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G51EmailSenderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g51emailsenderapi/G51EmailSenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g51emailsenderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G51EmailSenderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
